7f7102f7a03e5286faf914b3607ca35721eb330a
goish135 <goish2468@gmail.com>
2020-04-04 12:39:58 +0800

    加上navibar&當日標記

--- status.php
+++ status.php
@@ -48,8 +48,115 @@ a:hover {
   background-color: #4CAF50;
   color: white;
 }
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: red;
+}
+
+.active {
+  background-color: #FFBB00;
+}
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color: red;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 6px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+  
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+  
+}
 </style>
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+   <div class="dropdown">
+    <button class="dropbtn">Classroom 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="status.php?class=<?php echo "EC1005" ?>">EC1005</a>
+      <a href="#">EC1006</a>
+      <a href="#">EC1014-2</a>
+      <a href="#">EC2013-1</a>
+      <a href="#">EC2013-2</a>
+      <a href="#">EC2015</a>
+      <a href="#">EC3015</a>
+      <a href="#">EC3016</a>
+      <a href="#">EC5000</a>
+      <a href="#">EC5007</a>
+      <a href="#">EC5012</a>
+      <a href="#">EC5025</a>
+      <a href="#">EC5026</a>
+      <a href="#">EC9014</a>
+      <a href="#">EC9032-1</a>
+      <a href="#">EC9032-2</a>
+      <a href="#">EC9013</a>
+    </div>
+  </div> 
+  <li><a href="Request.php">Request</a></li>
+  <li><a href="Contact.html">Contact</a></li>
+  <li><a href="About.html">About</a></li>
+</ul>
+<br>
 <?php
+$sdd = date("m-d");
 if(isset($_REQUEST['date']))
 {
     //echo $_REQUEST['date'];
@@ -65,6 +172,7 @@ else
     date_default_timezone_set('Asia/Taipei');
     //當前日期
     $sdefaultDate = date("Y-m-d");
+    
     // echo $sdefaultDate;
     // echo $sdefaultDate.'<br>';
     
@@ -220,10 +328,26 @@ for($i=0;$i<7;$i++)
     echo '<table id="customers">';
     echo '<th>'.$Arr[0].'</th>';
     echo '<th>'.$Arr[1].'</th>';
+    //$b = date('Y-m-d',(strtotime("$tmp")+(86400*$i)));
+    //echo $b;
+    //if($sdefaultDate==$b)
+    {    
+                    // echo $b;
+                    //echo "today";
+                    //echo "<th style='border-color:red;border-style:solid;border-width:3px;padding:5px;'>".$Arr[$i]." ".$a."</th>";
+                    
+    }
     
+    //else
+        
     for($i=2;$i<9;$i++)
     {
-        echo '<th>'.$Arr[$i].' '.$wk[$i-2].'</th>';
+        if($sdd==$wk[$i-2])
+        {
+            echo "<th style='border-color:red;border-style:solid;border-width:3px;padding:5px;'>".$Arr[$i].' '.$wk[$i-2]."</th>";
+        }
+        else
+            echo '<th>'.$Arr[$i].' '.$wk[$i-2].'</th>';
     }
     $s = ['A','1','2','3','4','B','5','6','7','8','9','C','D','E','F'];
     $t = ['7:00-7:50','8:10-9:00','9:10-10:00','10:10-11:00','11:10-12:00','12:10-13:00','13:10-14:00','14:10-15:00','15:10-16:00','16:10-17:00','17:10-18:00','18:20-19:10','19:15-20:05','20:10-21:00','21:05-21:55'];
